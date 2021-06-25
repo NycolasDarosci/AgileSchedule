2332b194b66a856bb312838464305f0c6a41312c
Nycolas Darosci <nycoollaas@gmail.com>
2021-06-25 10:55:11 -0300

    modificando swagger

--- src/main/java/br/com/agileschedule/config/SwaggerConfig.java
+++ src/main/java/br/com/agileschedule/config/SwaggerConfig.java
@@ -28,7 +28,7 @@ public class SwaggerConfig {
     private ApiInfo metaInfo() {
         ApiInfo apiInfo = new ApiInfo("AgileSchedule", "Sistema de gerenciamento dos estagiarios",
                 "Criado pela Squad-4", "Terms",
-                new Contact("Sptint4-SpringBoot", "https://github.com/nizzorto/agileschedule", null), null, null,
+                new Contact("Sptint4-SpringBoot", "https://github.com/NycolasDarosci/AgileSchedule", null), null, null,
                 new ArrayList<VendorExtension>());
         return apiInfo;
 
