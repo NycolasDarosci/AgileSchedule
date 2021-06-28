154ab3c0dfc66f14167ef7d9a1888ae71c5edfc0
Nycolas Darosci <nycoollaas@gmail.com>
2021-06-28 17:03:05 -0300

    método validar data

--- src/main/java/br/com/agileschedule/config/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/agileschedule/config/AutenticacaoViaTokenFilter.java
@@ -9,17 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import br.com.agileschedule.entity.User;
 import br.com.agileschedule.repository.UserRepository;
 
-
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 
 	private TokenService tokenService;
 	private UserRepository clienteRepository;
-	
+
 	public AutenticacaoViaTokenFilter(TokenService tokenService, UserRepository repository) {
 		this.tokenService = tokenService;
 		this.clienteRepository = repository;
@@ -30,27 +30,26 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 			throws ServletException, IOException {
 		String token = recuperaToken(request);
 		boolean valido = tokenService.isTokenValido(token);
-		if(valido) {
+		if (valido) {
 			autenticarCliente(token);
 		}
-		
 	}
-	
+
 	private void autenticarCliente(String token) {
 
 		Long idUsuario = tokenService.getIdCliente(token);
 		User cliente = clienteRepository.findById(idUsuario).get();
-		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(cliente, null, cliente.getAuthorities());
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(cliente, null,
+				cliente.getAuthorities());
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
 
 	private String recuperaToken(HttpServletRequest request) {
 		String token = request.getHeader("Authorization");
-		if(token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+		if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
 			return null;
 		}
 		return token.substring(7, token.length());
 	}
 
-	
 }
--- src/main/java/br/com/agileschedule/config/SecurityConfiguration.java
+++ src/main/java/br/com/agileschedule/config/SecurityConfiguration.java
@@ -9,49 +9,44 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.com.agileschedule.repository.UserRepository;
 
-
-
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private AutenticacaoService autenticacaoService;
-	
+
 	@Autowired
 	private TokenService service;
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/").permitAll()
-		.antMatchers(HttpMethod.GET ,"/calendario").permitAll()
-		.antMatchers(HttpMethod.POST ,"/newCalendar").permitAll()
-		.antMatchers(HttpMethod.PUT , "/calendario/*").permitAll()
-		.antMatchers(HttpMethod.DELETE , "/calendario/*").permitAll()
-		.antMatchers("/calendario/*").permitAll()
-		.antMatchers(HttpMethod.POST ,"/cadastro").permitAll()
-		.anyRequest().authenticated()
-		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().addFilterBefore(new AutenticacaoViaTokenFilter(service, repository), UsernamePasswordAuthenticationFilter.class);
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers(HttpMethod.GET, "/calendario").permitAll()
+				.antMatchers(HttpMethod.POST, "/newCalendar").permitAll().antMatchers(HttpMethod.PUT, "/calendario/*")
+				.permitAll().antMatchers(HttpMethod.DELETE, "/calendario/*").permitAll().antMatchers("/calendario/*")
+				.permitAll().antMatchers(HttpMethod.POST, "/cadastro").permitAll().anyRequest().authenticated().and()
+				.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.addFilterBefore(new AutenticacaoViaTokenFilter(service, repository),
+						UsernamePasswordAuthenticationFilter.class);
 	}
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**.html","/css/cadastro.css","/css/Style.css" ,"/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+		web.ignoring().antMatchers("/**.html", "/css/cadastro.css", "/css/Style.css", "/v2/api-docs", "/webjars/**",
+				"/configuration/**", "/swagger-resources/**");
 	}
 }
